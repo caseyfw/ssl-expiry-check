700e6051d366181ed109b405dcfcd9583a001988
Casey Fulton <casey_fulton@flightcentre.com>
2018-06-28 11:11:43 +1000

    Added error checking.

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 $domain = $_GET['domain'] ?? 'www.apple.com';
 $expiryText = trim(shell_exec("echo | openssl s_client -connect $domain:443 2>/dev/null | openssl x509 -noout -enddate | sed -n 's/.*=\(.*\)/\\1/p'"));
 
+header('Content-type: application/json');
+
+if (empty($expiryText)) {
+    echo json_encode(['domain' => $domain, 'error' => 'no ssl']);
+    exit;
+}
+
 $expiryDate = DateTime::createFromFormat('M d H:i:s Y e', $expiryText);
 $diff = (new DateTime())->diff($expiryDate);
+$expiryText = $diff > 0 ? $diff->format('in %a days') : $diff->format('%a days ago');
 
-header('Content-type: application/json');
 echo json_encode([
-  'domain' => $domain,
-  'expires' => $expiryDate->format('Y-m-d g:i:s'),
-  'expires-text' => $diff > 0 ? $diff->format('in %a days') : $diff->format('%a days ago'),
+    'domain' => $domain,
+    'expires' => $expiryDate->format('Y-m-d g:i:s'),
+    'expires-text' => $expiryText,
 ]);
